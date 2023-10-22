021e6c409dd5582559d771c7a7748384b3a4dd75
dczech <dczech@asco.com.pl>
2023-10-22 17:25:39 +0200

    feat: implemented API request, changed app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import { useState } from "react";
-import MainMenu from "./components/MainMenu";
+import { useMemo, useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import AppRouter from "./components/Routes";
+import AppContext, { AppContextInterface } from "./AppContext";
 import "./App.css";
-import Router from "./components/Routes";
-import { RouterProvider } from "react-router-dom";
 
 function App() {
+  const [accessToken, setAccessToken] = useState(window.localStorage.getItem('AUTH_TOKEN') ?? undefined);
+  const appContextValue: AppContextInterface = useMemo(() => ({
+    accessToken,
+    setAccessToken: (token) => {
+      if (token) {
+        window.localStorage.setItem('AUTH_TOKEN', token);
+      } else {
+        window.localStorage.removeItem('AUTH_TOKEN');
+      }
+
+      setAccessToken(token);
+    },
+  }), [accessToken]);
+  
   return (
-    <div className="p-3 mb-2 bg-dark text-white">
-      <RouterProvider router={Router}></RouterProvider>
-    </div>
+    <BrowserRouter>
+      <div className="p-3 mb-2 bg-dark text-white">
+        <AppContext.Provider value={appContextValue}>
+          <AppRouter />
+        </AppContext.Provider>
+      </div>
+    </BrowserRouter>
   );
 }
 
