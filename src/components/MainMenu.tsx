843ce56e2ff44771401af602ca6300a12f711046
dawidczech355a <dawidczech355a@gmail.com>
2023-10-23 23:30:46 +0200

    feat: requests implementation for classification and todays game

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,106 +1,178 @@
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styled from "styled-components";
-import "../mainMenu.css";
-import { useState } from 'react';
+import styled from 'styled-components';
+import { AxiosError } from 'axios';
 
-const classification = [
-    {id: "1", name: "Jula", position: 1, games:{ win:2, draw:1, lose:0 }, points: 7, subPoints:130},
-    {id: "2", name: "Paweł", position: 2, games:{ win:1, draw:1, lose:1 }, points: 4, subPoints:100},
-    {id: "3", name: "Dawid", position: 3, games:{ win:0, draw:0, lose:3 }, points: 0, subPoints:30},
-    {id: "4", name: "Gabi", position: 4, games:{ win:2, draw:1, lose:0 }, points: 7, subPoints:130},
-    {id: "5", name: "Grzegorz", position: 5, games:{ win:1, draw:1, lose:1 }, points: 4, subPoints:100},
-    {id: "6", name: "Ola", position: 6, games:{ win:0, draw:0, lose:3 }, points: 0, subPoints:30}
-]
- 
-const todayGames = [
-    {id: "1", players: [ {id:"1", name:"Jula", isFinished:true}, {id:"2", name:"Paweł", isFinished:true} ]},
-    {id: "2", players: [ {id:"3", name:"Dawid", isFinished:true}, {id:"6", name:"Ola", isFinished:true} ]},
-    {id: "3", players: [ {id:"5", name:"Grzegorz", isFinished:true}, {id:"4", name:"Gabi", isFinished:true} ]}
-];
+import '../mainMenu.css';
+import { useRequest } from '../hooks/useRequest';
+
+export interface PlayersInterface {
+	id: string;
+	name: string;
+	isFinished: boolean;
+}
+
+export interface TodayGamesInterface {
+	id: string;
+	players: PlayersInterface[];
+}
+
+export interface GamesInterface {
+	won: number;
+	draw: number;
+	lose: number;
+}
+
+export interface ClassificationInterface {
+	id: string;
+	name: string;
+	position: number;
+	games: GamesInterface;
+	points: number;
+	subPoints: number;
+}
 
 const Content = styled.div`
-    display: flex;
+	display: flex;
 `;
 
 const TodayTable = styled.table`
-    width : 100%;
-`
+	width: 100%;
+`;
 
 const TodayTablePlayers = styled.td`
-    width : 45%;
-`
+	width: 45%;
+`;
 
 const PlayButton = styled.button`
-    
-    width : 50%;
-    height : 50%
-`
-
+	width: 50%;
+	height: 50%;
+`;
 
 export default function MainMenu() {
-    const navigate = useNavigate();
-    const current = new Date();
-    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
-  
+	const [todayGames, setTodayGames] = useState<TodayGamesInterface[] | undefined>();
+	const [classification, setClassification] = useState<ClassificationInterface[] | undefined>();
+	const [todayGameMessage, setTodayGameMessage] = useState<string>();
+	const [classificationMessage, setClassificationMessage] = useState<string>();
 
-    return (
-        <div className='Container'>
-            <h1>Siemaneczko!</h1>
-            <Content className='inner-container'>
-                <div className='tables'>
-                    <table className="table table-striped table-hover main-table">
-                        <thead className='table-dark' >
-                            <tr className='table-headers'>
-                                <th className='position' scope="col" >#</th>
-                                <th className='name' scope="col">Imię</th>
-                                <th scope="col">Punkty</th>
-                                <th scope="col">Małe Punkty</th>
-                                <th scope="col">Wygrane</th>
-                                <th scope="col">Remisy</th>
-                                <th scope="col">Przegrane</th>
-                            </tr>
-                        </thead>
-                        <tbody className='table-dark'>
-                            {
-                                classification.map(({id,position, name, games,points, subPoints}) => (
-                                    <tr key={id}>
-                                        <th className='position' scope="row">{position}</th>
-                                        <td className='name'>{name}</td>
-                                        <td>{points}</td>
-                                        <td>{subPoints}</td>
-                                        <td>{games.win}</td>
-                                        <td>{games.draw}</td>
-                                        <td>{games.lose}</td>
-                                    </tr>
-                                    )
-                                )
-                            }
-                        </tbody>
-                    </table>
-                    <TodayTable>
-                        <thead>
-                            <tr>
-                            <th scope="col" colSpan={3}>{date}</th>
-                            </tr>
-                        </thead>
-                        <tbody className="table-group-divider">
-                            {
-                                todayGames.map(({id, players}) => (
-                                    <tr>
-                                        <TodayTablePlayers>{players[0].name}</TodayTablePlayers>
-                                        <td>vs</td>
-                                        <TodayTablePlayers>{players[1].name}</TodayTablePlayers>
-                                    </tr>
-                                    )
-                                )
-                            }
-                        </tbody>
-                    </TodayTable>
-                </div>
-                    <PlayButton type="button" className="button-play btn btn-warning" onClick={() =>navigate("/play")}>
-                        Zagraj
-                    </PlayButton>
-            </Content>
-        </div>
-    )
+	const request = useRequest();
+	const navigate = useNavigate();
+
+	const now = new Date();
+	const date = `${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()}`;
+
+	useEffect(function onMountFetchTodayGames() {
+		const fetchData = async () => {
+			try {
+				const result = await request.get('/games/today');
+
+				if (result.data) {
+					setTodayGames(result.data);
+				}
+			} catch (error) {
+				if (error instanceof AxiosError) {
+					if (error.response?.data.message) {
+						setTodayGameMessage(error.response.data.message);
+					}
+				}
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	useEffect(function onMountFetchClassification() {
+		const fetchData = async () => {
+			try {
+				const result = await request.get('/users/classification');
+
+				if (result.data) {
+					setClassification(result.data);
+				}
+			} catch (error) {
+				if (error instanceof AxiosError) {
+					if (error.response?.data.message) {
+						setClassificationMessage(error.response.data.message);
+					}
+				}
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	return (
+		<div className='Container'>
+			<h1>Siemaneczko!</h1>
+			<Content className='inner-container'>
+				<div className='tables'>
+					{classification && classification.length > 0 ? (
+						<table className='table table-striped table-hover main-table'>
+							<thead className='table-dark'>
+								<tr className='table-headers'>
+									<th className='position' scope='col'>
+										#
+									</th>
+									<th className='name' scope='col'>
+										Imię
+									</th>
+									<th scope='col'>Punkty</th>
+									<th scope='col'>Małe Punkty</th>
+									<th scope='col'>Wygrane</th>
+									<th scope='col'>Remisy</th>
+									<th scope='col'>Przegrane</th>
+								</tr>
+							</thead>
+							<tbody className='table-dark'>
+								{classification.map(({ id, position, name, games, points, subPoints }) => (
+									<tr key={id}>
+										<th className='position' scope='row'>
+											{position}
+										</th>
+										<td className='name'>{name}</td>
+										<td>{points}</td>
+										<td>{subPoints}</td>
+										<td>{games.won}</td>
+										<td>{games.draw}</td>
+										<td>{games.lose}</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					) : (
+						<div style={{ color: 'red' }}>{classificationMessage}</div>
+					)}
+					{todayGames && todayGames.length > 0 ? (
+						<TodayTable>
+							<thead>
+								<tr>
+									<th scope='col' colSpan={3}>
+										{date}
+									</th>
+								</tr>
+							</thead>
+							<tbody className='table-group-divider'>
+								{todayGames.map(({ players }) => (
+									<tr>
+										<TodayTablePlayers>{players[0].name}</TodayTablePlayers>
+										<td>vs</td>
+										<TodayTablePlayers>{players[1].name}</TodayTablePlayers>
+									</tr>
+								))}
+							</tbody>
+						</TodayTable>
+					) : (
+						<div style={{ color: 'red' }}>{todayGameMessage}</div>
+					)}
+				</div>
+				<PlayButton
+					type='button'
+					className='button-play btn btn-warning'
+					onClick={() => navigate('/play')}
+				>
+					Zagraj
+				</PlayButton>
+			</Content>
+		</div>
+	);
 }
