a9ff7d0883621c3e2e3cdf84c25f337c78cf493c
dawidczech355a <dawidczech355a@gmail.com>
2023-10-24 22:37:04 +0200

    feat: pobieranie pytan i wysylanie odpowiedzi

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,38 +1,37 @@
-import axios from "axios";
-import { useAppContext } from "./useAppContext";
+import axios from 'axios';
+import { useAppContext } from './useAppContext';
 
 // TYMCZASOWE STAŁE - DOMYŚLNIE W .ENV
 const BASE_URL = 'http://localhost:5000';
 
+// TODO: retry 3 razy!!
 const request = axios.create({
-  baseURL: BASE_URL,
+	baseURL: BASE_URL,
 });
 
 export function useRequest() {
-  const { accessToken, setAccessToken } = useAppContext();
-  
-  request.interceptors.request.use(
-    req => {
-      if (accessToken) {
-        req.headers.Authorization = accessToken;
-      }
-  
-      return req;
-    }
-  );
-  
-  request.interceptors.response.use(
-    response => {
-      return response; 
-    },
-    error => {      
-      if (error.response.status === 401) {
-        setAccessToken(undefined);
-      }
+	const { accessToken, setAccessToken } = useAppContext();
 
-      throw error;
-    }
-  )
-  
-  return request;
+	request.interceptors.request.use((req) => {
+		if (accessToken) {
+			req.headers.Authorization = accessToken;
+		}
+
+		return req;
+	});
+
+	request.interceptors.response.use(
+		(response) => {
+			return response;
+		},
+		(error) => {
+			if (error.response.status === 401) {
+				setAccessToken(undefined);
+			}
+
+			throw error;
+		}
+	);
+
+	return request;
 }
