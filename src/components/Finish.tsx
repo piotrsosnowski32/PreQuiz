94058c81f0d47bd5e837717fb80c0e63a6b77191
dawidczech355a <dawidczech355a@gmail.com>
2023-10-28 03:13:21 +0200

    fix: game logic changes

--- src/components/Finish.tsx
+++ src/components/Finish.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { devices } from './constants';
 
@@ -7,51 +7,53 @@ const MainDiv = styled.div`
     width: 100%
     align-items: center;
 
-`
+`;
 
 const Info = styled.p`
-    width: 100%;
-    height: 100px;
-    text-align: center;
-    font-size: 25px;
-    @media only screen and ${devices.md} {
-        font-size: 50px;
-    }
-`
+	width: 100%;
+	height: 100px;
+	text-align: center;
+	font-size: 25px;
+	@media only screen and ${devices.md} {
+		font-size: 50px;
+	}
+`;
 
 const EndButton = styled.button`
-    width: 150px;
+	width: 150px;
 	height: 50px;
-`
+`;
 
 const ButtonDiv = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
 
 const MemikDiv = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
 
 export default function Finish() {
-    const navigate = useNavigate();
-return (
-    <MainDiv>
-        <Info>Dzięki za dzisiaj!</Info>
+	const navigate = useNavigate();
 
-        <MemikDiv>
-            TU MEMIK NA DZIEŃ
-        </MemikDiv>
+	return (
+		<MainDiv>
+			<Info>Dzięki za dzisiaj!</Info>
 
-        <ButtonDiv>
-            <EndButton type='button' className='button-play btn btn-warning' onClick={() => navigate('/')}>
-                 KONIEC
-            </EndButton>
-        </ButtonDiv>
-        
-    </MainDiv>
-);
+			<MemikDiv>TU MEMIK NA DZIEŃ</MemikDiv>
+
+			<ButtonDiv>
+				<EndButton
+					type='button'
+					className='button-play btn btn-warning'
+					onClick={() => navigate('/')}
+				>
+					KONIEC
+				</EndButton>
+			</ButtonDiv>
+		</MainDiv>
+	);
 }
